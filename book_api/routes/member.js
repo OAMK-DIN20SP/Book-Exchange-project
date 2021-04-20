19c10d883542ddc5f839e4fe7a191dd6e0846504
Thuc Nguyen <t0ngdi00@students.oamk.fi>
2021-04-20 11:11:14 +0700

    save

--- book_api/routes/member.js
+++ book_api/routes/member.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var book = require('../models/book_model.js');
 var member = require('../models/member_model.js');
 var async = require('async');
+var multer = require('multer');
+const helpers = require('../helpers');
+const path = require('path');
 
 router.get('/', (req, res) => {
     const { idmember } = req.query;
@@ -88,6 +91,64 @@ router.post('/login2', (req, res) => { // res w/ json, no redirect
         }
     });
     
+});
+
+// var storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, '/images/avatars')
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.fieldname + '-' + Date.now())
+//   }
+// });
+ 
+// var upload = multer({ storage: storage });
+
+// router.post('/uploadavatar', upload.single('myFile'), (req, res, next) => {
+//   const file = req.file
+//   if (!file) {
+//     const error = new Error('Please upload a file')
+//     error.httpStatusCode = 400
+//     return next(error)
+//   }
+//   res.send(file)
+// });
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, '/images/avatars/');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+router.post('/uploadavatar', (req, res) => {
+    // 'myFile' is the name of our file input field in the HTML form
+    let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('myFile');
+
+    upload(req, res, function(err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`);
+    });
 });
 
 module.exports = router;
